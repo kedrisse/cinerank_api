8c2fb3fa001554871c0f8f4522b26656a1b37f9d
Clément Bosc <clement.bosc@outlook.com>
2018-07-05 17:00:54 +0200

    poster colored box shadow

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,4 +3,15 @@ document.addEventListener('DOMContentLoaded', function () {
     var instances = M.FormSelect.init(elems, {});
     var tabs = document.querySelectorAll('.tabs');
     var instance = M.Tabs.init(tabs, {});
+
+    var images = document.querySelectorAll('.movie-poster');
+
+    for (var i = 0; i < images.length; i++) {
+        var vibrant = new Vibrant(images[i]);
+        var swatches = vibrant.swatches();
+        var rgb = swatches.DarkVibrant.rgb;
+        images[i].style.boxShadow = 'rgba('+rgb[0]+', '+rgb[1]+', '+rgb[2]+', 0.54) 0px 3px 17px 4px';
+    }
 });
+
+
